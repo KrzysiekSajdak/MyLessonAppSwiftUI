7d2e4552df5007f2ffe1eba8f3d91ccc4b4c7118
KrzysiekSajdak <krzysztofsajdak@outlook.com>
2020-08-07 06:52:09 +0200

    Initial Commit

---
+++ LessonApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LessonApp
+//
+//  Created by Krzysztof  Sajdak  on 07/08/2020.
+//  Copyright © 2020 Krzysztof  Sajdak . All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
